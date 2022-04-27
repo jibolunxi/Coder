a4c3ab724c207158415b70c4d1243cc9e7cfddac
changjie <2193927906@qq.com>
2022-04-27 15:51:24 +0800

    add mybatis

--- src/test/java/nom/cj/coder/CoderApplicationTests.java
+++ src/test/java/nom/cj/coder/CoderApplicationTests.java
@@ -1,13 +1,33 @@
 package nom.cj.coder;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @SpringBootTest
 class CoderApplicationTests {
 
+	@Autowired
+	DataSource dataSource;
+
+	@Test
+	void testMybatis() {
+		Connection conn;
+		try {
+			conn = dataSource.getConnection();
+			System.err.println(conn);
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+	}
+
 	@Test
 	void contextLoads() {
 	}
+
 
 }
